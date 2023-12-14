c0ca18ac7742f8bf6e8507a3aa819c13b7b62954
Davidmide02 <99823023+Davidmide02@users.noreply.github.com>
2023-12-14 16:54:22 +0100

    second commit

---
+++ components/card.jsx
@@ -0,0 +1,21 @@
+const Card = ()=> {
+// const 
+
+    return (
+        <div className="card">
+            <Image src=""/>
+            <div className="texts">
+                <h6>{title}</h6>
+                <div className="desr">
+                    <div className="icon-text">
+                        {/* map here */}
+                        <Image src="image here"/>
+
+                        <p>text here</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
